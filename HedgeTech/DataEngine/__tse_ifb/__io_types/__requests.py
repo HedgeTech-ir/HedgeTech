d8deba13e82450bde3f12933923de60ac529029b
mtrshirazi21 <84249699+mtrshirazi21@users.noreply.github.com>
2025-11-20 23:53:34 +0330

    add doc to type class

--- HedgeTech/DataEngine/__tse_ifb/__io_types/__requests.py
+++ HedgeTech/DataEngine/__tse_ifb/__io_types/__requests.py
@@ -14,12 +14,34 @@ from typing import (
 
 
 class SymbolNames(TypedDict):
+    """
+    Represents a collection of instrument symbol names for batch API requests.
+
+    Attributes:
+        symbol_names (List[str]): A list of instrument symbol names (strings) to be queried.
+
+    Example:
+        >>> symbols: SymbolNames = {"symbol_names": ["ETF001", "FUT002", "STK003"]}
+        >>> symbols["symbol_names"][0]
+        'ETF001'
+    """
     symbol_names: List[str]
 
 # +--------------------------------------------------------------------------------------+ #
 
 
 class SymbolIsins(TypedDict):
+    """
+    Represents a collection of instrument ISIN identifiers for batch API requests.
+
+    Attributes:
+        symbol_isins (List[str]): A list of instrument ISIN codes (strings) to be queried.
+
+    Example:
+        >>> isins: SymbolIsins = {"symbol_isins": ["IR0001234567", "IR0009876543"]}
+        >>> isins["symbol_isins"][1]
+        'IR0009876543'
+    """
     symbol_isins: List[str]
     
 # +--------------------------------------------------------------------------------------+ #
